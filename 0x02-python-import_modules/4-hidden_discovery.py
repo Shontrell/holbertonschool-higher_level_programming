cc6489920b4f44cc6dd6fab23f8bf59c6c3438d4
shontrell <shontrell.scott3@gmail.com>
2020-05-01 00:37:37 +0000

    prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in range(len(dir(hidden_4)[1])):
+    for i in range(0, len(dir(hidden_4))):
         if (dir(hidden_4)[i][0] != '_') and (dir(hidden_4)[i][1] != '_'):
             print(dir(hidden_4)[i])
