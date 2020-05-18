9d0237cdb8c2093cdbd3d3f029b8fb9c3f902f87
shontrell <shontrell.scott3@gmail.com>
2020-05-18 19:40:41 +0000

    divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,11 @@ def safe_print_division(a, b):
     divide = 0
     try:
         divide = a / b
+    except:
+        pass
     finally:
         if divide:
-            print("Inside result: {:f}".format(divide))
+            print("Inside result: {}".format(divide))
             return (divide)
         else:
             print("Inside result: None")
