fc9ed683d11585b9b31080ec539758413bdc7ed0
shontrell <shontrell.scott3@gmail.com>
2020-05-20 04:21:25 +0000

    Write a class Square that defines a square by private attribute size and raise errors and return area for size given

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""defining class Square"""
+
+
+class Square:
+    """private instance attribute: size"""
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of the square
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        retrieving size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        setting size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def area(self):
+        """
+        public instance method:
+            area()
+        Return:
+            current square area
+        """
+        return ((self.__size) * (self.__size))
