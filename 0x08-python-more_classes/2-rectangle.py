e2585f479104eabff2b58017aeb9bb416b2f4c37
shontrell <shontrell.scott3@gmail.com>
2020-05-26 01:54:52 +0000

    Creates a class Rectangle that defines a rectangle by width, height, area \

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Creates a class Rectangle that defines a rectangle by width, height, area \
+and perimeter
+"""
+
+
+class Rectangle:
+    """
+    Args:
+        width: integer
+        height: integer
+    Raises:
+        TypeError: width must be an integer
+        TypeError: height must be an integer
+        ValueError: width must be >= 0
+        ValueError: height must be >= 0
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return (0)
+        return ((2 * self.width) + (2 * self.height))
