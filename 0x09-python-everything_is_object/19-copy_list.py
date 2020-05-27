3c2bfbb38cef3a188cb9af2fa6eb51b32acb13cc
shontrell <shontrell.scott3@gmail.com>
2020-05-27 01:42:26 +0000

    a function that returns a copy of a list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-""" a function that returns a copy of a list"""
-
-
 def copy_list(l):
-    """
-    Args:
-        l: list
-    Return:
-        copy of a list
-    """
     return l.copy()
