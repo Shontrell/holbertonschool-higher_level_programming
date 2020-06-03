b28edb3f42692436d9b0ee91c23466c920e5704b
shontrell <shontrell.scott3@gmail.com>
2020-06-03 02:25:11 +0000

    adding PIM: def area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creates class BaseGeometry"""
+
+
+class BaseGeometry:
+    """adding public instance method: def area() """
+    def __init__(self):
+        pass
+    def area(self):
+        raise Exception("area() is not implemented")
