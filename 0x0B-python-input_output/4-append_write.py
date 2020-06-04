0e18b82db83dbb713af953980d861ce99b1e1bd4
shontrell <shontrell.scott3@gmail.com>
2020-06-04 14:48:16 +0000

    adding comments

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -6,6 +6,13 @@ file and returns the number of characters added
 
 
 def append_write(filename="", text=""):
+    """
+    Args:
+        filename: name of file
+        text: string to be appended at end of text file
+    Return:
+        the number of characters added
+    """
     chnumb = 0
     with open(filename, 'a') as x:
         chnumb = x.write(text)
