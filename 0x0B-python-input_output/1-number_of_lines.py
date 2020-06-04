572d9a76bb70fa68eea2b66d5b62365826efd050
shontrell <shontrell.scott3@gmail.com>
2020-06-04 14:30:43 +0000

    adding comments

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,6 +3,12 @@
 
 
 def number_of_lines(filename=""):
+    """
+    Args:
+        filename: name of file
+    Return:
+        number of lines of a text file
+    """
     lc = 0
     with open(filename) as x:
         for line in x:
