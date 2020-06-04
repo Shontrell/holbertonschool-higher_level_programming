211cfb633eb224d7b22742ea5002ec9cec638f0d
shontrell <shontrell.scott3@gmail.com>
2020-06-04 14:43:01 +0000

    adding comments

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,6 +6,13 @@ the number of characters written
 
 
 def write_file(filename="", text=""):
+    """
+    Args:
+        filename: name of file
+        text: string being written to text file
+    Return:
+        the number of characters written
+    """
     chnumb = 0
     with open(filename, 'w') as x:
         chnumb = x.write(text)
