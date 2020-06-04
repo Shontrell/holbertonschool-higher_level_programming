34beac84857e6da49841515eb5d9756044e883b3
shontrell <shontrell.scott3@gmail.com>
2020-06-04 14:54:04 +0000

    adding comments

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -6,5 +6,11 @@ represented by a JSON string
 
 
 def from_json_string(my_str):
+    """
+    Args:
+        my_str: string
+    Return:
+        an object (Python data structure) represented by a JSON string
+    """
     import json
     return json.loads(my_str)
