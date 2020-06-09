f6c935553946f5b7672543032e0541e4eb62d2c4
shontrell <shontrell.scott3@gmail.com>
2020-06-09 22:03:02 +0000

    test file for class Base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,3 +18,20 @@ class test_Base(unittest.TestCase):
         result = pep8style.check_files(['file1.py', 'file2.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    def test_idset(self):
+        """
+        Testing to see if id is set
+        """
+        tb = Base()
+        self.assertEqual(tb.id, 1)
+        tb2 = Base(2)
+        self.assertEqual(tb2.id, 2)
+
+    def test_to_json_string(self):
+        """
+        Testing to_json_string method
+        """
+        testlist = [{'id': 2, 'x': 5, 'y': 6}]
+        self.assertCountEqual(Base.to_json_string(testlist),
+                              '[{'id': 2, 'x': 5, 'y': 6}]')
