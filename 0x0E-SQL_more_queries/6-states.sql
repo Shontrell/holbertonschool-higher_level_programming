75e1ef86186538158631a8f914cb833b6a36b6e7
shontrell <shontrell.scott3@gmail.com>
2020-07-15 03:10:34 +0000

    a script that creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- a script that creates the database hbtn_0d_usa and the table states
+-- If the table states already exists, your script should not fail
+-- If the database hbtn_0d_usa already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT
+NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
